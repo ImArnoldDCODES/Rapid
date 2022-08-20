38cd8624f1bf957b172379bfd8c4434e20f9d8a0
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-08-20 12:13:50 -0700

    clean

--- src/App.js
+++ src/App.js
@@ -11,56 +11,58 @@ function App() {
 
   const [finalPoint, setFinalPoint] = useState("")
 
-    useEffect(() => {
-      fetchMe()
-      return () => {
-      };
-    }, [finalPoint]);
+  useEffect(() => {
+    fetchMe()
+    return () => {
+    };
+  }, [finalPoint]);
 
 
   const fetchMe = () => {
 
-  fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${endPoint}`, {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'b17ea89733msh92a5bbff3f3f518p138afbjsn2b1007c93b44',
-      'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-    }
-  })
-    .then(response => { 
-      return response.json()} 
-    )
-    .then(data => {
-      setContainer(data.d)
+    fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${endPoint}`, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'b17ea89733msh92a5bbff3f3f518p138afbjsn2b1007c93b44',
+        'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+      }
     })
-    .catch(err => console.error(err));
+
+      .then(response => {
+        return response.json()
+      }
+      )
+      .then(data => {
+        setContainer(data.d)
+      })
+      .catch(err => console.error(err));
   }
 
-    const onchangehandler = (e) =>{
-      setEndPoint(e.target.value)
-    }
+  const onchangehandler = (e) => {
+    setEndPoint(e.target.value)
+  }
 
-    const submithandler = (e) => {
-      e.preventDefault();
-      setFinalPoint(endPoint);
-    }
-  
+  const submithandler = (e) => {
+    e.preventDefault();
+    setFinalPoint(endPoint);
+  }
+
 
   return (
     <div className="App">
-     <form onSubmit={submithandler}>
-      <input type="text" value={endPoint} onChange={onchangehandler}/>
-      <button type='submit'>Submit</button>
-     </form>
+      <form onSubmit={submithandler}>
+        <input type="text" value={endPoint} onChange={onchangehandler} />
+        <button type='submit'>Submit</button>
+      </form>
 
-     {container.map((item) => {
-      return (
-        <div key={item.id}>
-          <img src={item.i.imageUrl} alt={item.i.imageUrl}/>
-        <p>{item.l}</p>
-        </div>
-      )
-     })}
+      {container.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.i.imageUrl} alt={item.i.imageUrl} />
+            <p>{item.l}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
